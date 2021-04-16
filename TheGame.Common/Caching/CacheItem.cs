186f280b152566912f51b5a3bc198be29cf8c3ea
cristianomartinsdias82 <crisdi@gmail.com>
2021-04-16 20:08:09 -0300

    Under construction

---
+++ TheGame.Common/Caching/CacheItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TheGame.Common.Caching
+{
+    public struct CacheItem<T>
+    {
+        public readonly Guid Id;
+        public T Item { get; private set; }
+
+        private CacheItem(T item)
+        {
+            Id = Guid.NewGuid();
+            Item = item;
+        }
+
+        public static CacheItem<T> Create(T item)
+        => new CacheItem<T>(item);
+    }
+
+    //public class CacheItem<T>
+    //{
+    //    public Guid Id { get; set; } = Guid.NewGuid();
+
+    //    public T Item { get; private set; }
+
+    //    private CacheItem(T item)
+    //    {
+    //        Item = item;
+    //    }
+    //    public static CacheItem<T> Create(T item)
+    //        => new CacheItem<T>(item);
+    //}
+}
